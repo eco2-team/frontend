55ddd459aaaa10222d4249e234bfb1f6cc7bcdfa
suji8073 <suzy8073@naver.com>
2025-12-01 02:33:33 +0900

    [fix] Toast Message safe-area-inset-top 고려해서 뜨도록 높이 조절

--- src/components/Toast/ToastContainer.tsx
+++ src/components/Toast/ToastContainer.tsx
@@ -18,7 +18,7 @@ export const ToastContainer = () => {
   const Icon = currentToast.type === 'success' ? SuccessIcon : ErrorIcon;
 
   return (
-    <div className='fixed top-4 right-0 left-0 z-50 flex justify-center'>
+    <div className='fixed top-[calc(var(--safe-area-top)+4px)] right-0 left-0 z-50 flex justify-center'>
       <div className='animate-slide-down flex items-center gap-5 rounded-[10px] bg-black/80 px-5 py-3 shadow-lg'>
         <img src={Icon} alt={currentToast.type} className='h-6 w-6' />
         <span className='text-xs leading-5 font-medium tracking-[-0.15px] whitespace-pre-line text-white'>
